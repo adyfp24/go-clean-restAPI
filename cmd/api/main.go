332ad663c6a7e1a92726e1f17a969870054fbb30
adyfp24 <adyfp24@gmail.com>
2024-09-23 13:26:07 +0700

    refactor: setup folder config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"go-clean-restAPI/config"
+	"go-clean-restAPI/internal/handler"
+	"go-clean-restAPI/internal/repository"
+	"go-clean-restAPI/internal/route"
+	"go-clean-restAPI/internal/usecase"
 	"go-clean-restAPI/pkg/database"
 	"log"
+	"net/http"
 )
 
 func main() {
+	config.LoadConfig()
+
 	if err := database.InitDB(); err != nil {
 		log.Fatalf("Could not initialize database: %v", err)
 	}
 
-	fmt.Println("wkwkw golang ni bos")
+	repositories := repository.NewRepositories(database.DB)
+	usecases := usecase.NewUseCases(repositories)
+	handlers := handler.NewHandlers(usecases)
+
+	r := route.InitRoute(handlers)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatalf("Could not start server: %v", err)
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,52 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Database string
+}
+
+type ServerConfig struct {
+	Port string
+}
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+}
+
+var AppConfig *Config
+
+func LoadConfig() {
+	if err := godotenv.Load; err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	AppConfig = &Config{
+		Server: ServerConfig{
+			Port: getEnv("PORT", "3000"),
+		},
+		Database: DatabaseConfig{
+			Host:     getEnv("DB_HOST", "127.0.0.1"),
+			Port:     getEnv("DB_PORT", "3000"),
+			Username: getEnv("DB_USER", "root"),
+			Password: getEnv("DB_PASSWORD", ""),
+			Database: getEnv("DB_NAME", "db_clean_go"),
+		},
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exist := os.LookupEnv(key); exist {
+		return value
+	}
+	return defaultValue
+}
